18eaac3a872150c17a67f77e7e044316e615aaf1
Jakub Kovář <6195502+minop@users.noreply.github.com>
2023-04-14 09:09:14 +0200

    app skeleton

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {DropFile, FD_PETRI_NET, PetriNet, PetriNetParserService} from "ilpn-components";
+import {ReplaySubject} from "rxjs";
+
 
 @Component({
     selector: 'app-root',
@@ -6,4 +9,27 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+    public FD_PN = FD_PETRI_NET;
+
+    public svgHeight = '800px';
+
+    public net$: ReplaySubject<PetriNet>;
+
+    constructor(private parser: PetriNetParserService) {
+        this.net$ = new ReplaySubject<PetriNet>(1);
+    }
+
+    public onNetUpload(files: Array<DropFile>) {
+        if (files.length === 0) {
+            console.debug('no files');
+            return;
+        }
+
+        const net = this.parser.parse(files[0].content);
+        if (net !== undefined) {
+            console.debug(net);
+            this.net$.next(net);
+        }
+    }
 }
